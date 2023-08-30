60f8be10fab8fccb2021fcfbf2c2bb2d3e894dbb
danieltb92 <daniel.tunjano92@gmail.com>
2023-08-29 19:02:18 -0500

    v1

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
 (function () {
     const sliders = [...document.querySelectorAll (".slider-body")];
     console.log(sliders)
-    const selector = document.querySelector ('.slider-prev-next-control')
+    const selector = [...document.querySelectorAll (".pagination-slider input")];
     console.log (selector)
 
-    selector.addEventListener('click', ()=>changePosition(1))
-
+    selector.addEventListener("click", console.log("hola"));
+// ()=>changePosition(1)
+    // function changePosition(change){
+    //     console.log (change)
+    //     const currentElement = Number(document.querySelector('.img-show').
+    //     dataset.id)
+    // }
+    // console.log (currentElement)
+    
 
 })()
