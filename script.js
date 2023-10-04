9fc01af15350b9db7a4af185f04314faf2dba754
danieltb92 <daniel.tunjano92@gmail.com>
2023-10-03 19:53:44 -0500

    v1.2.1 update

--- script.js
+++ script.js
@@ -1,18 +1,41 @@
-(function () {
-    const sliders = [document.querySelectorAll (".slider-body")];
-    console.log(sliders)
-    const selector = [document.querySelectorAll (".pagination-slider input")];
-    console.log (selector)
 
-    selector.addEventListener("click", console.log("hola"));
-// ()=>changePosition(1)
-    // function changePosition(change){
-    //     console.log (change)
-    //     const currentElement = Number(document.querySelector('.img-show').
-    //     dataset.id)
-    // }
-    // console.log (currentElement)
-    
- 
-})()
- 
+function siguienteImagen() {
+    const carrusel = document.querySelector(".inner");
+    const imagenes = carrusel.querySelectorAll(".slider-body");
+    const imagenActual = carrusel.querySelector(".img-show.slider-body");
+    const siguienteImagen = imagenActual.nextElementSibling;
+
+    if (!siguienteImagen) {
+      siguienteImagen = imagenes[0];
+    }
+  
+    imagenActual.classList.remove("img-show");
+    imagenActual.classList.add("img-hidden");
+    siguienteImagen.classList.add("img-show");
+
+  }
+
+  function anteriorImagen() {
+    const carrusel = document.querySelector(".inner");
+    const imagenes = carrusel.querySelectorAll(".slider-body");
+    const imagenActual = carrusel.querySelector(".slider-body.img-show");
+    const anteriorImagen = imagenActual.previousElementSibling;
+  
+    if (!anteriorImagen) {
+      anteriorImagen = imagenes[imagenes.length - 1];
+    }
+  
+    anteriorImagen.classList.add("img-show");
+    imagenActual.classList.remove("img-show");
+    imagenActual.classList.add("img-hidden");
+  }
+  
+//   // Eventos
+  
+  document.querySelector(".siguiente").addEventListener("click", siguienteImagen);
+  document.querySelector(".anterior").addEventListener("click", anteriorImagen);
+  
+  // Inicialización
+
+  const imagenActual = document.querySelector(".slider-body:first-child");
+  imagenActual.classList.add("img-show");
